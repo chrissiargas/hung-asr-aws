c318b64451cd06170a95fc1cc8b61775a2be6e2e
c.siargkas <chrissiargas@gmail.com>
2026-06-23 16:19:47 +0300

    layer-wise fusion

--- config/parser.py
+++ config/parser.py
@@ -138,6 +138,7 @@ class Parser:
         self.dual_fuse_args.downsamplers = args.dual_fuse_args['downsamplers']
         self.dual_fuse_args.positional_info = args.dual_fuse_args['positional_info']
         self.dual_fuse_args.layer_wise_fusion = args.dual_fuse_args['layer_wise_fusion']
+        self.dual_fuse_args.layer_weights_static = args.dual_fuse_args['layer_static_weights']
 
         ## LoRA Configurations
         self.dual_fuse_args.linguistic_lora = args.dual_fuse_args['linguistic_lora']
