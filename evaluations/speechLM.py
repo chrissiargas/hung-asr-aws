c272431a7b648dd4f34a086ce393c9840f4a3563
c.siargkas <chrissiargas@gmail.com>
2026-07-27 15:05:13 +0300

    Vamvou commit

--- evaluations/speechLM.py
+++ evaluations/speechLM.py
@@ -297,8 +297,6 @@ def evaluate(info, dataset, split='test', device='cuda', iters = None):
 
     split_manager = splitter()
     data = split_manager.split(datasets=dataset_names, splitting=False)
-    evaluation_data = get_data(data[split], bad_folder, iters=iters, normalized=False, has_duration=True, filters=FILTERS, split=split)
-    evaluation_data = concatenate(evaluation_data)
 
     evaluation_data = get_data(data[split],
                           bad_folder,
@@ -308,6 +306,8 @@ def evaluate(info, dataset, split='test', device='cuda', iters = None):
                           randomize=args.randomize,
                           has_duration=True,
                           normalize_type=args.normalize)
+
+    evaluation_data = concatenate(evaluation_data)
 
     print(f"Evaluating on {dataset_names} ({len(evaluation_data)} samples)...")
     evaluate_model(evaluation_data, conf, args, info, checkpoint_path, checkpoint_dir, device)
