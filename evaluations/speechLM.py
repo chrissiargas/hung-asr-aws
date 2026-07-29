84a88035fcfbaeac0f4aed94c36cfc5d11b501bc
c.siargkas <chrissiargas@gmail.com>
2026-07-29 13:36:36 +0300

    eng+de+fin commit

--- evaluations/speechLM.py
+++ evaluations/speechLM.py
@@ -276,7 +276,7 @@ def get_results_path(conf, info, dataset, split='test', data_folder: bool = True
 
     return results_path
 
-def get_bad_folder_path(conf, dataset: str, split: str):
+def get_bad_folder_path(conf):
     bad_folder_path = os.path.join(os.path.expanduser('~'),
                                    conf.dataset_path,
                                    conf.language,
@@ -288,7 +288,7 @@ def evaluate(info, dataset, split='test', device='cuda', iters = None):
     conf, args, _, _, checkpoint_path, checkpoint_dir, _ = init(info, restart=False)
 
     info['res_folder'] = get_results_path(conf, info, dataset, split)
-    bad_folder = get_bad_folder_path(conf, dataset, split)
+    bad_folder = get_bad_folder_path(conf)
 
     if not isinstance(dataset, list):
         dataset_names = [dataset]
@@ -411,9 +411,10 @@ if __name__ == "__main__":
             if args_dict['concat_test']:
                 args_dict['test_dataset'] = DATASETS
                 evaluate(args_dict, DATASETS, device=device)
-            for dataset in DATASETS:
-                args_dict['test_dataset'] = dataset
-                evaluate(args_dict, dataset, device=device)
+            else:
+                for dataset in DATASETS:
+                    args_dict['test_dataset'] = dataset
+                    evaluate(args_dict, dataset, device=device)
 
             wandb.finish()
 
